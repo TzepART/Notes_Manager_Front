7d8340fb75847e0d00d80814bab7b4a7d30f23f7
TzepART <tzepart@gmail.com>
2016-06-05 02:56:46 +0400

    setting for minimization css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,6 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('default', function () {
-    return gulp.src('src/style.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('css/'));
-});
-
 var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer');
+    cssnano = require('gulp-cssnano');
 
 gulp.task('autoprefix - плагин', function () {
     return gulp.src('src/css/sass-style.css')
@@ -20,4 +9,10 @@ gulp.task('autoprefix - плагин', function () {
             cascade: false
         }))
         .pipe(gulp.dest('src/css/'));
+});
+
+gulp.task('nano ', function() {
+    return gulp.src('src/css/style.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('css/'));
 });
