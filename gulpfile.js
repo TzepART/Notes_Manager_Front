e20b9695432fd5cbd4a03475ee31f01af6056bfb
TzepART <tzepart@gmail.com>
2016-06-05 14:33:36 +0400

    setting for auto-reload css after change .sass and .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer');
     cssnano = require('gulp-cssnano');
+    // livereload = require('gulp-livereload');
 
 gulp.task('autoprefix - плагин', function () {
     return gulp.src('src/css/sass-style.css')
@@ -11,8 +12,15 @@ gulp.task('autoprefix - плагин', function () {
         .pipe(gulp.dest('src/css/'));
 });
 
-gulp.task('nano ', function() {
+gulp.task('nano', function() {
     return gulp.src('src/css/style.css')
         .pipe(cssnano())
         .pipe(gulp.dest('css/'));
+});
+
+gulp.task('default', ['nano']);
+
+gulp.task('watch', function() {
+    // watch all source for rebuild
+    gulp.watch('src/css/**/*', ['default']);
 });
