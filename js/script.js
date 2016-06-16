6945072fabf9664496e952c4fa076d8526b0b2a4
TzepART <tzepart@gmail.com>
2016-06-17 01:57:20 +0400

    Canvas. Gradient. createRadialGradient

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 function draw() {
     var ctx = document.getElementById('canvas').getContext('2d');
-    var lineJoin = ['round','bevel','miter'];
-    ctx.lineWidth = 10;
-    for (var i=0;i<lineJoin.length;i++){
-        ctx.lineJoin = lineJoin[i];
-        ctx.beginPath();
-        ctx.moveTo(-5,5+i*40);
-        ctx.lineTo(35,45+i*40);
-        ctx.lineTo(75,5+i*40);
-        ctx.lineTo(115,45+i*40);
-        ctx.lineTo(155,5+i*40);
-        ctx.stroke();
-    }
+
+    // Create gradients
+    var radgrad = ctx.createRadialGradient(45,45,10,52,50,30);
+    radgrad.addColorStop(0, '#A7D30C');
+    radgrad.addColorStop(0.9, '#019F62');
+    radgrad.addColorStop(1, 'rgba(1,159,98,0)');
+
+    var radgrad2 = ctx.createRadialGradient(105,105,20,112,120,50);
+    radgrad2.addColorStop(0, '#FF5F98');
+    radgrad2.addColorStop(0.75, '#FF0188');
+    radgrad2.addColorStop(1, 'rgba(255,1,136,0)');
+
+    var radgrad3 = ctx.createRadialGradient(95,15,15,102,20,40);
+    radgrad3.addColorStop(0, '#00C9FF');
+    radgrad3.addColorStop(0.8, '#00B5E2');
+    radgrad3.addColorStop(1, 'rgba(0,201,255,0)');
+
+    var radgrad4 = ctx.createRadialGradient(0,150,50,0,140,90);
+    radgrad4.addColorStop(0, '#F4F201');
+    radgrad4.addColorStop(0.8, '#E4C700');
+    radgrad4.addColorStop(1, 'rgba(228,199,0,0)');
+
+    // draw shapes
+    ctx.fillStyle = radgrad4;
+    ctx.fillRect(0,0,150,150);
+    ctx.fillStyle = radgrad3;
+    ctx.fillRect(0,0,150,150);
+    ctx.fillStyle = radgrad2;
+    ctx.fillRect(0,0,150,150);
+    ctx.fillStyle = radgrad;
+    ctx.fillRect(0,0,150,150);
 }
 
 
