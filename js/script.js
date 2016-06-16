2cd7125311b15e31b1f24cc743ee37808e0a4ee8
TzepART <tzepart@gmail.com>
2016-06-17 01:55:29 +0400

    Canvas. Lines. lineJoin

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 function draw() {
     var ctx = document.getElementById('canvas').getContext('2d');
-    for (var i = 0; i < 10; i++){
-        ctx.lineWidth = 1+i;
+    var lineJoin = ['round','bevel','miter'];
+    ctx.lineWidth = 10;
+    for (var i=0;i<lineJoin.length;i++){
+        ctx.lineJoin = lineJoin[i];
         ctx.beginPath();
-        ctx.moveTo(5+i*14,5);
-        ctx.lineTo(5+i*14,140);
+        ctx.moveTo(-5,5+i*40);
+        ctx.lineTo(35,45+i*40);
+        ctx.lineTo(75,5+i*40);
+        ctx.lineTo(115,45+i*40);
+        ctx.lineTo(155,5+i*40);
         ctx.stroke();
     }
 }
