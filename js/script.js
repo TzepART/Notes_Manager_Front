a3ea3ea0c4deb776239611759f05d00f4317a25e
TzepART <tzepart@gmail.com>
2016-06-26 00:59:19 +0400

    Add circles sector with using canvas

--- js/script.js
+++ js/script.js
@@ -1,39 +1,93 @@
-function draw() {
-    var ctx = document.getElementById('canvas').getContext('2d');
+$('canvas')
+    .drawSlice({
+        layer: true,
+        name: 'blue-slice',
+        groups: ['chart', 'slices'],
+        fillStyle: 'blue',
+        x: 200, y: 200,
+        start: -45, end: 15,
+        radius: 200,
+        click: function(layer) {
+            console.log("blue");
+        }
+    })
+    .drawSlice({
+        layer: true,
+        name: 'green-slice',
+        groups: ['chart', 'slices'],
+        fillStyle: 'green',
+        x: 200, y: 200,
+        start: 15, end: 90,
+        radius: 200,
+    })
+    .drawSlice({
+        layer: true,
+        name: 'black-slice',
+        groups: ['chart', 'slices'],
+        fillStyle: 'black',
+        x: 200, y: 200,
+        start: 90, end: -45,
+        radius: 200,
+    })
+    .drawSlice({
+        layer: true,
+        name: 'red-slice',
+        groups: ['chart', 'slices'],
+        fillStyle: '#c33',
+        x: 200, y: 200,
+        start: -45, end: 15,
+        radius: 100,
+        click: function(layer) {
+            console.log("red");
+        }
+    })
+    .drawSlice({
+        layer: true,
+        name: 'grey-slice',
+        groups: ['chart', 'slices'],
+        fillStyle: 'grey',
+        x: 200, y: 200,
+        start: 15, end: 90,
+        radius: 100,
+    })
+    .drawSlice({
+        layer: true,
+        name: 'orange-slice',
+        groups: ['chart', 'slices'],
+        fillStyle: 'orange',
+        x: 200, y: 200,
+        start: 90, end: -45,
+        radius: 100,
+    });
 
-    // Create gradients
-    var radgrad = ctx.createRadialGradient(45,45,10,52,50,30);
-    radgrad.addColorStop(0, '#A7D30C');
-    radgrad.addColorStop(0.9, '#019F62');
-    radgrad.addColorStop(1, 'rgba(1,159,98,0)');
+/*$('canvas').drawLine({
+    layer: true,
+    draggable: true,
+    strokeStyle: '#000',
+    strokeWidth: 5,
+    rounded: true,
+    x1: 150, y1: 200,
+    x2: 150, y2: 0,
+    restrictDragToAxis: 'x',
+    click: function(layer) {
+        console.log(layer.x1);
+        console.log(layer);
+        this.x1 = 150;
+        this.y1 = 200;
+    }
+});*/
 
-    var radgrad2 = ctx.createRadialGradient(105,105,20,112,120,50);
-    radgrad2.addColorStop(0, '#FF5F98');
-    radgrad2.addColorStop(0.75, '#FF0188');
-    radgrad2.addColorStop(1, 'rgba(255,1,136,0)');
-
-    var radgrad3 = ctx.createRadialGradient(95,15,15,102,20,40);
-    radgrad3.addColorStop(0, '#00C9FF');
-    radgrad3.addColorStop(0.8, '#00B5E2');
-    radgrad3.addColorStop(1, 'rgba(0,201,255,0)');
-
-    var radgrad4 = ctx.createRadialGradient(0,150,50,0,140,90);
-    radgrad4.addColorStop(0, '#F4F201');
-    radgrad4.addColorStop(0.8, '#E4C700');
-    radgrad4.addColorStop(1, 'rgba(228,199,0,0)');
-
-    // draw shapes
-    ctx.fillStyle = radgrad4;
-    ctx.fillRect(0,0,150,150);
-    ctx.fillStyle = radgrad3;
-    ctx.fillRect(0,0,150,150);
-    ctx.fillStyle = radgrad2;
-    ctx.fillRect(0,0,150,150);
-    ctx.fillStyle = radgrad;
-    ctx.fillRect(0,0,150,150);
-}
-
-
-$(document).ready(function() {
-    draw();
+$('canvas').drawArc({
+    layer: true,
+    draggable: true,
+    fillStyle: '#36c',
+    x: 200, y: 150,
+    radius: 10,
+    data: {'id':22},
+    click: function(layer) {
+        console.log(layer.x1);
+        console.log(layer);
+        this.x1 = 150;
+        this.y1 = 200;
+    }
 });
