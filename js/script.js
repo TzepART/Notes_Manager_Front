63192fe137a58cb31d2baa49e4037270a9f92918
TzepART <tzepart@gmail.com>
2016-06-26 18:58:59 +0400

    Add function for change color by layers

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var CenterX = 300;
 var CenterY = 300;
+var bigRadius = 200;
 /**
  * Из декартовой в полярную систему координат.
  *
@@ -39,6 +40,81 @@ function cartesian2Dec(radius, degr) {
     return decCoor;
 }
 
+/**
+*function for convert HEX -> rgba
+ * */
+
+function hexInArray(h){
+    var m = h.slice(1).match(/.{2}/g);
+    m[0]=parseInt(m[0], 16);
+    m[1]=parseInt(m[1], 16);
+    m[2]=parseInt(m[2], 16);
+    return m;
+};
+
+function hexArrayInRgbString(m) {
+    var rgb = 'rgb('+m[0]+', '+m[1]+', '+m[2]+')';
+    return rgb;
+}
+
+function changeColorLayers(color,numLayers) {
+    var arColor = hexInArray(color);
+    var tempColor = arColor;
+    var arRBA = [];
+    var i = 0;
+    var difColor = (256-arColor[0])/numLayers;
+    var red = arColor[0] + difColor;
+    for(red; red <= 256; red = red + difColor){
+        tempColor[0] = Math.floor(red);
+        console.log(tempColor);
+        console.log(red);
+        arRBA[i] = hexArrayInRgbString(tempColor);
+        i++;
+    }
+    return arRBA;
+}
+
+console.log(changeColorLayers("#80407b",4));
+
+function createSector(data) {
+    $('canvas')
+        .drawSlice({
+            layer: true,
+            name: 'blue-slice',
+            groups: ['chart', 'slices'],
+            fillStyle: data.color,
+            x: CenterX, y: CenterY,
+            start: data.beginAngle, end: data.endAngle,
+            radius: 200,
+            strokeStyle: '#f60',
+            strokeWidth: 3,
+        })
+        .drawSlice({
+            layer: true,
+            name: 'red-slice',
+            groups: ['chart', 'slices'],
+            fillStyle: '#c33',
+            x: CenterX, y: CenterY,
+            start: -45, end: 15,
+            radius: 100,
+            strokeStyle: '#f60',
+            strokeWidth: 3,
+        })
+        .drawText({
+            layer: true,
+            fillStyle: '#c33',
+            fontFamily: 'Trebuchet MS, sans-serif',
+            fontSize: 18,
+            text: 'Творчество',
+            x: CenterX, y: CenterY,
+            radius: 220,
+            rotate: -15,
+            dblclick: function(layer) {
+                console.log("Sector");
+                $('#pop_sector').css('display','block').attr('id',555);
+            },
+        });
+}
 
 $('canvas')
     .drawSlice({
@@ -55,6 +131,20 @@ $('canvas')
             console.log("blue");
         }
     })
+    .drawSlice({
+        layer: true,
+        name: 'red-slice',
+        groups: ['chart', 'slices'],
+        fillStyle: '#c33',
+        x: CenterX, y: CenterY,
+        start: -45, end: 15,
+        radius: 100,
+        strokeStyle: '#f60',
+        strokeWidth: 3,
+        click: function(layer) {
+            console.log("red");
+        }
+    })
     .drawText({
         layer: true,
         fillStyle: '#c33',
@@ -63,7 +153,11 @@ $('canvas')
         text: 'Творчество',
         x: CenterX, y: CenterY,
         radius: 220,
-        rotate: -15
+        rotate: -15,
+        dblclick: function(layer) {
+            console.log("Sector");
+            $('#pop_sector').css('display','block').attr('id',555);
+        },
     })
     .drawSlice({
         layer: true,
@@ -139,20 +233,6 @@ $('canvas')
     })
     .drawSlice({
         layer: true,
-        name: 'red-slice',
-        groups: ['chart', 'slices'],
-        fillStyle: '#c33',
-        x: CenterX, y: CenterY,
-        start: -45, end: 15,
-        radius: 100,
-        strokeStyle: '#f60',
-        strokeWidth: 3,
-        click: function(layer) {
-            console.log("red");
-        }
-    })
-    .drawSlice({
-        layer: true,
         name: 'grey-slice',
         groups: ['chart', 'slices'],
         fillStyle: 'grey',
@@ -220,12 +300,104 @@ $('canvas').drawArc({
     //     console.log('mousemove');
     // },
     mouseover: function(layer) {
+        var pol = cartesian2Polar(layer.x, layer.y);
+        var dec = cartesian2Dec(bigRadius+30,pol.degr);
+        $('canvas').drawArc({
+                layer: true,
+                strokeStyle: '#48D1CC',
+                strokeWidth: 3,
+                name: 'circle1',
+                x: CenterX, y: CenterY,
+                radius: pol.distance,
+        });
+        $('canvas').drawLine({
+            strokeWidth: 3,
+            name: 'line1',
+            strokeStyle: '#48D1CC',
+            x1: CenterX, y1: CenterY,
+            x2: dec.X, y2: dec.Y,
+        });
         var Label = $('canvas').getLayer('myLabel');
         Label.fillStyle = "Red";
     },
     mouseout: function(layer) {
         var Label = $('canvas').getLayer('myLabel');
-        Label.fillStyle = '#36c';
+        $('canvas').removeLayer('circle1');
+        $('canvas').removeLayer('line1');
+    },
+    // dragstart: function(layer) {
+    //     console.log('dragstart');
+    // },
+    dblclick: function(layer) {
+        $('#pop_lable').css('display','block').attr('id',555);
+    },
+    // dragstop: function(layer) {
+    //     console.log('dragstop');
+    // },
+    // touchstart: function(layer) {
+    //     console.log('touchstart');
+    // },
+    // touchend: function(layer) {
+    //     console.log('touchend');
+    // },
+    // touchmove: function(layer) {
+    //     console.log('touchmove');
+    // }
+});
+
+$('canvas').drawArc({
+    layer: true,
+    draggable: true,
+    name: 'myLabel2',
+    fillStyle: '#36c',
+    x: CenterX+40, y: 200,
+    radius: 10,
+    data: {'id':22},
+    dragstop: function(layer) {
+        console.log('X - '+layer.x);
+        console.log('Y - '+layer.y);
+        var pol = cartesian2Polar(layer.x, layer.y);
+        console.log(pol);
+        var dec = cartesian2Dec(pol.distance,pol.degr);
+        console.log(dec);
+    },
+    // dblclick: function(layer) {
+    //     console.log('dblclick');
+    // },
+    // mousedown: function(layer) {
+    //     console.log('mousedown');
+    // },
+    // mouseup: function(layer) {
+    //     console.log('mouseup');
+    // },
+    // mousemove: function(layer) {
+    //     console.log('mousemove');
+    // },
+    mouseover: function(layer) {
+        var pol = cartesian2Polar(layer.x, layer.y);
+        var dec = cartesian2Dec(bigRadius+30,pol.degr);
+        $('canvas').drawArc({
+            layer: true,
+            strokeStyle: '#48D1CC',
+            strokeWidth: 3,
+            name: 'circle2',
+            x: CenterX, y: CenterY,
+            radius: pol.distance,
+        });
+        $('canvas').drawLine({
+            strokeWidth: 3,
+            name: 'line2',
+            strokeStyle: '#48D1CC',
+            x1: CenterX, y1: CenterY,
+            x2: dec.X, y2: dec.Y,
+        });
+        var Label = $('canvas').getLayer('myLabel2');
+        Label.fillStyle = "Red";
+    },
+    mouseout: function(layer) {
+        var Label = $('canvas').getLayer('myLabel2');
+        $('canvas').removeLayer('circle2');
+        $('canvas').removeLayer('line2');
     },
     // dragstart: function(layer) {
     //     console.log('dragstart');
