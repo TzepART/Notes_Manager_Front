c59dfbb11b5e498d92715593264de0dc5fe305e9
TzepART <tzepart@gmail.com>
2016-06-26 04:23:00 +0400

    Add text along an arc

--- js/script.js
+++ js/script.js
@@ -4,37 +4,64 @@ $('canvas')
         name: 'blue-slice',
         groups: ['chart', 'slices'],
         fillStyle: 'blue',
-        x: 200, y: 200,
+        x: 300, y: 300,
         start: -45, end: 15,
         radius: 200,
         click: function(layer) {
             console.log("blue");
         }
     })
+    .drawText({
+        fillStyle: '#c33',
+        fontFamily: 'Ubuntu, sans-serif',
+        fontSize: 18,
+        text: 'Творчество',
+        x: 300, y: 300,
+        radius: 220,
+        rotate: -40
+    })
     .drawSlice({
         layer: true,
         name: 'green-slice',
         groups: ['chart', 'slices'],
         fillStyle: 'green',
-        x: 200, y: 200,
+        x: 300, y: 300,
         start: 15, end: 90,
         radius: 200,
+    })
+    .drawText({
+        fillStyle: '#c33',
+        fontFamily: 'Ubuntu, sans-serif',
+        fontSize: 18,
+        text: 'Семья',
+        x: 300, y: 300,
+        radius: 220,
+        rotate: 25
     })
     .drawSlice({
         layer: true,
         name: 'black-slice',
         groups: ['chart', 'slices'],
         fillStyle: 'black',
-        x: 200, y: 200,
+        x: 300, y: 300,
         start: 90, end: -45,
         radius: 200,
+    })
+    .drawText({
+        fillStyle: '#c33',
+        fontFamily: 'Ubuntu, sans-serif',
+        fontSize: 18,
+        text: 'Отдых',
+        x: 300, y: 300,
+        radius: 220,
+        rotate: 100
     })
     .drawSlice({
         layer: true,
         name: 'red-slice',
         groups: ['chart', 'slices'],
         fillStyle: '#c33',
-        x: 200, y: 200,
+        x: 300, y: 300,
         start: -45, end: 15,
         radius: 100,
         click: function(layer) {
@@ -46,7 +73,7 @@ $('canvas')
         name: 'grey-slice',
         groups: ['chart', 'slices'],
         fillStyle: 'grey',
-        x: 200, y: 200,
+        x: 300, y: 300,
         start: 15, end: 90,
         radius: 100,
     })
@@ -55,7 +82,7 @@ $('canvas')
         name: 'orange-slice',
         groups: ['chart', 'slices'],
         fillStyle: 'orange',
-        x: 200, y: 200,
+        x: 300, y: 300,
         start: 90, end: -45,
         radius: 100,
     });
@@ -81,13 +108,49 @@ $('canvas').drawArc({
     layer: true,
     draggable: true,
     fillStyle: '#36c',
-    x: 200, y: 150,
+    x: 300, y: 150,
     radius: 10,
     data: {'id':22},
-    click: function(layer) {
-        console.log(layer.x1);
-        console.log(layer);
+    dragstop: function(layer) {
+        console.log(layer.x);
+        console.log(layer.y);
         this.x1 = 150;
         this.y1 = 200;
-    }
+    },
+    // dblclick: function(layer) {
+    //     console.log('dblclick');
+    // },
+    // mousedown: function(layer) {
+    //     console.log('mousedown');
+    // },
+    // mouseup: function(layer) {
+    //     console.log('mouseup');
+    // },
+    // mousemove: function(layer) {
+    //     console.log('mousemove');
+    // },
+    // mouseover: function(layer) {
+    //     console.log('mouseover');
+    // },
+    // mouseout: function(layer) {
+    //     console.log('mouseout');
+    // },
+    // dragstart: function(layer) {
+    //     console.log('dragstart');
+    // },
+    dblclick: function(layer) {
+        $('#pop_lable').css('display','block').attr('id',555);
+    },
+    // dragstop: function(layer) {
+    //     console.log('dragstop');
+    // },
+    // touchstart: function(layer) {
+    //     console.log('touchstart');
+    // },
+    // touchend: function(layer) {
+    //     console.log('touchend');
+    // },
+    // touchmove: function(layer) {
+    //     console.log('touchmove');
+    // }
 });
